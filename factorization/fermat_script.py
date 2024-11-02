a76e105479b70c685d6494791c147d77371d03f2
louayykaltoum <140081367+louayykaltoum@users.noreply.github.com>
2024-11-02 14:34:40 +0100

    fermat

---
+++ factorization/fermat_script.py
@@ -0,0 +1,24 @@
+from sympy import *
+from Crypto.Util.number import getPrime
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser("Fermat factorization")
+    parser.add_argument("-n", type=int, help="number to factorize")
+    return parser.parse_args()
+
+def fermat(n):
+    a = int(n ** 0.5)
+    for i in range(2**50):
+        temp = a+i 
+        if ( isprime(temp) and n%temp == 0 and isprime(n//temp)):
+            return temp , n // temp
+def main():
+    args = get_args()
+    p, q = fermat(args.n)
+    print(f'p: {p}\n',
+          f'q: {q}\n')
+
+if __name__ == "__main__":
+    main()
